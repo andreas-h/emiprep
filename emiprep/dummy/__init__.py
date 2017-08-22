ced9df568b82d8c072085a71aa45217cb7ff815b
Andreas Hilboll <hilboll@uni-bremen.de>
2017-08-22 17:00:30 +0200

    DOC: add API documentation

---
+++ emiprep/dummy/__init__.py
@@ -0,0 +1,26 @@
+"""
+emiprep.dummy
+=============
+
+.. currentmodule:: emiprep.dummy
+
+.. autosummary::
+   :toctree: api/
+
+   Dummyfunc
+
+"""
+
+def Dummyfunc(my_input):
+    """This is just a dummy function
+
+    Parameters
+    ----------
+    my_input : int
+
+    Returns
+    -------
+    my_output : int
+
+    """
+    return 22
